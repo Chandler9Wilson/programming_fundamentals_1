428754fc757435d617a3897a33f66458d3f0cb9b
Chandler Wilson <chandler9wilson@gmail.com>
2018-05-30 10:56:47 -0500

    init

---
+++ print_errors_fixed_lab_1/TT18_L1_Wilson.py
@@ -0,0 +1,56 @@
+# Name:
+# COSC1336, Lab 1, Fix print syntax errors
+#
+# An easy first program prints out a few words, like: Hello, world!
+# But when learning a brand new language, even something
+# "easy" can be difficult. Find and fix any errors below.
+#
+# There should be a space between words in your output.
+# Each statement displays some text.
+# Repair each output instruction so it outputs correctly.
+# If it is NOT broken, don't fix it.
+# These can be repaired with the proper use of the print function
+# and the proper use of string __deliminators__ << typo?.
+
+# On that last sentance I believe you mean delimiters.
+
+print let's get started
+print "does this work?"
+print (working or not)
+print ("how about this?)
+print (is this right?)
+print ("Can you see me now?")
+print [how many errors can you find?]
+print 'success, I think'
+print ("What if I put a period on the end?").
+print ("What if I put a semicolon on the end?");
+print ("What could possibly be wrong"}
+print ("this" "that" "and" "the" "other")
+print ("one")("two")("three")  # keep these all on one line of output
+print ("tic") print("tak") print("toe")  # keep these all on one line of code
+prnt "anybody knows that prnt means print
+print_now(''I cannot wait any longer'')
+print < good grief! This is messed up, too!>
+(try it this way!!)print
+("Who cares where the verb goes?")print
+("Start")print("End")
+display("give me a shout when it works")
+output: don't mess up on me now!
+show -> Please just do what I say!
+put >> "The best is yet to come."
+cout << "In C++, this works. What about python?";
+print ("Can I do this??")
+print (If this were easy, anyone could do it!]
+print("Quote the raven: "Nevermore" ")  # put in quotes
+print('She said: "Where's my cell phone? I've got to find it!"')
+print(Here are four quote marks: """")
+println(<How could something so easy be so difficult?>)
+PRINT("does it matter uppercase or lowercase?")
+Print 'this ought to work'
+p_r_i_n_t ("One letter at a time.
+out = That is about enough of this. # print variable out
+"Good-bye, and good luck."
+
+# Paste your output below:
+# Hint: You can "comment out" your output using:
+# Format -> Comment Out Region
---
+++ print_errors_fixed_lab_1/first_try_TT18_L1_Wilson.py
@@ -0,0 +1,99 @@
+# Name: Chandler Wilson
+# COSC1336, Lab 1, Fix print syntax errors
+#
+# An easy first program prints out a few words, like: Hello, world!
+# But when learning a brand new language, even something
+# "easy" can be difficult. Find and fix any errors below.
+#
+# There should be a space between words in your output.
+# Each statement displays some text.
+# Repair each output instruction so it outputs correctly.
+# If it is NOT broken, don't fix it.
+# These can be repaired with the proper use of the print function
+# and the proper use of string deliminators.
+
+print("let's get started")
+print('does this work?')
+print('working or not')
+print('how about this?')
+print('is this right?')
+print('Can you see me now?')
+print('how many errors can you find?')
+print('success, I think')
+print('What if I put a period on the end?')
+print('What if I put a semicolon on the end?')
+print('What could possibly be wrong')
+print('this ' 'that ' 'and ' 'the ' 'other ')
+# keep these all on one line of output
+print('one', 'two', 'three')
+# keep these all on one line of output
+print('tic', 'tak', 'toe')
+print('anybody knows that prnt means print')
+print('I cannot wait any longer')
+print('good grief! This is messed up, too!')
+print('try it this way!!')
+print('Who cares where the verb goes?')
+print('Start' + ' ' + 'End')
+print('give me a shout when it works')
+print("don't mess up on me now!")
+print('Please just do what I say!')
+print('The best is yet to come.')
+print('In C++, this works. What about python?')
+print('Can I do this??')
+print('If this were easy, anyone could do it!')
+# put in quotes
+print("Quote the raven: 'Nevermore' ")
+print("She said: 'Where's my cell phone? I've got to find it!'")
+print("Here are four quote marks: ''''")
+print('How could something so easy be so difficult?')
+print('does it matter uppercase or lowercase?')
+print('this ought to work')
+print('One letter at a time.')
+# print variable out
+out = 'That is about enough of this.'
+print(out)
+print('Good-bye, and good luck.')
+
+
+# Paste your output below:
+# Hint: You can "comment out" your output using:
+# Format -> Comment Out Region
+
+""" Output below
+
+let's get started
+does this work?
+working or not
+how about this?
+is this right?
+Can you see me now?
+how many errors can you find?
+success, I think
+What if I put a period on the end?
+What if I put a semicolon on the end?
+What could possibly be wrong
+this that and the other
+one two three
+tic tak toe
+anybody knows that prnt means print
+I cannot wait any longer
+good grief! This is messed up, too!
+try it this way!!
+Who cares where the verb goes?
+StartEnd
+give me a shout when it works
+don't mess up on me now!
+Please just do what I say!
+The best is yet to come.
+In C++, this works. What about python?
+Can I do this??
+If this were easy, anyone could do it!
+Quote the raven: 'Nevermore'
+She said: 'Where's my cell phone? I've got to find it!'
+Here are four quote marks: ''''
+How could something so easy be so difficult?
+does it matter uppercase or lowercase?
+this ought to work
+One letter at a time.
+That is about enough of this.
+Good-bye, and good luck. """
