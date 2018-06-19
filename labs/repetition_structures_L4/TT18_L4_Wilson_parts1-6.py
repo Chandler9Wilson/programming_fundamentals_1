d6910c5b51ffd30e10cd7ffca3e8c7ea586a8299
Chandler Wilson <chandler9wilson@gmail.com>
2018-06-19 16:40:28 -0500

    Added austin_temperature_stat, removed need for exit()

--- labs/repetition_structures_L4/TT18_L4_Wilson_parts1-6.py
+++ labs/repetition_structures_L4/TT18_L4_Wilson_parts1-6.py
@@ -18,7 +18,7 @@ def hello_user():
     horizontal_rule()
     print('    1. Draw boxes')
     print('    2. Draw triangles')
-    print('    3. Play rock, paper, scissors')
+    print('    3. Compute statistics about Austin temperatures')
     print('    4. Choose a season')
     print('    5. Should you drive?')
     print('    6. Quit')
@@ -35,7 +35,7 @@ def hello_user():
         hello_user()
     elif user_input is '3':
         horizontal_rule
-        rock_paper_scissor()
+        austin_temperature_statistics()
         hello_user()
     elif user_input is '4':
         horizontal_rule()
@@ -49,7 +49,7 @@ def hello_user():
         horizontal_rule()
         print('Goodbye')
         # Exit the program (really just this function)
-        exit()
+        return
     else:
         horizontal_rule()
         print('Please enter a valid integer (1-6)')
@@ -108,7 +108,7 @@ def draw_boxes():
         horizontal_rule()
         draw_boxes()
     elif box_size == 0:
-        hello_user()
+        return
 
 
 def draw_triangles():
@@ -142,7 +142,74 @@ def draw_triangles():
         horizontal_rule()
         draw_triangles()
     elif triangle_size == 0:
-        hello_user()
+        return
+
+
+def austin_temperature_statistics():
+
+    def temp_input():
+        temp = input('Input a temperature (farenheit) to add to the list: ')
+        return temp
+
+    def valid_temp(temp):
+        if -5 <= temp <= 115:
+            return True
+        else:
+            return False
+
+    def input_and_validation():
+        temp = temp_input()
+
+        if temp in ['Q', 'q', 'quit']:
+            compute_stats()
+        else:
+            convert_error_message = 'ERROR: Please enter an integer for ' + \
+                'the temperature'
+            temp = convert_to_int(temp, input_and_validation,
+                                  convert_error_message)
+
+            if valid_temp(temp):
+                valid_temp_list.append(temp)
+                input_and_validation()
+            else:
+                invalid_temp_list.append(temp)
+                horizontal_rule()
+                print('ERROR: Please enter an integer between -5 and 115')
+                horizontal_rule()
+                input_and_validation()
+
+    def compute_stats():
+
+        def is_freezing(temp):
+            if temp <= 32:
+                return True
+            else:
+                return False
+
+        zero_base_offset = 1
+        temp_list_length = len(valid_temp_list)
+        sorted_temp_list = sorted(valid_temp_list)
+        average_temp = sum(sorted_temp_list) / temp_list_length
+        freezing_temp_list = list(filter(is_freezing, sorted_temp_list))
+
+        horizontal_rule()
+        print('    You entered', temp_list_length, 'temperatures')
+        print('    The highest temperature you entered was', sorted_temp_list[(
+            temp_list_length - zero_base_offset)], 'degrees farenheit')
+        print('    The lowest temperature you entered was',
+              sorted_temp_list[0], 'degrees farenheit')
+        print('    The average temperature was',
+              average_temp, 'degrees farenheit')
+        print('    There were', len(freezing_temp_list),
+              'temperatures below freezing')
+        print('    You entered', temp_list_length, 'valid temperatures')
+        print('    You entered', len(invalid_temp_list),
+              'invalid temperatures')
+
+    valid_temp_list = []
+    invalid_temp_list = []
+
+    input_and_validation()
 
 
 # Some utility functions
@@ -150,7 +217,7 @@ def draw_triangles():
 
 def convert_to_int(int_to_convert,
                    callback,
-                   error_message='ERROR: Please enter and integer'):
+                   error_message='ERROR: Please enter an integer'):
     try:
         return int(int_to_convert)
     except ValueError:
