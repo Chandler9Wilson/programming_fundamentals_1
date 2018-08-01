9f1a629b5eafade094b44ced481b337ff12dcbb0
Chandler Wilson <chandler9wilson@gmail.com>
2018-07-31 19:04:00 -0500

    Barebones init

---
+++ exams/third_exam/TT18_X3_Wilson.py
@@ -0,0 +1,27 @@
+# Name: Chandler Wilson
+# Date: 07/31/2018
+# COSC1336, Exam 3, 4 parts:
+#   part 1: notice, load_dictionary, and main functions
+#   part 2: spellcheck function
+#   part 3: enhance spellcheck
+#   part 4: status message (extra credit)
+
+
+def notice(exam_number):
+    notice_str = 'Spell checking program for Exam %s lab' % (exam_number)
+
+    return notice_str
+
+
+def input_loop():
+    return
+
+
+def main():
+    print(notice(3))
+    input_loop()
+    print('\nGood bye!')
+
+
+if __name__ == "__main__":
+    main()
